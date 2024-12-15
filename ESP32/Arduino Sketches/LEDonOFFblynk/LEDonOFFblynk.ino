bb9b41c081a876e42ca427b40a77f607c10b95ab
Muaaz <muaazamjad007@gmail.com>
2024-12-15 23:32:11 +0100

    added ESP32 led blink via WIFI IOT project

---
+++ ESP32/Arduino Sketches/LEDonOFFblynk/LEDonOFFblynk.ino
@@ -0,0 +1,26 @@
+#define BLYNK_TEMPLATE_ID "TMPL4vV0gEzw5"
+#define BLYNK_TEMPLATE_NAME "LED"
+#define BLYNK_AUTH_TOKEN "bcvfWaGfd05yBOvkHBrN3H37W7prVLSr"
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Liwest_58bc";
+char pass[] = "15112020";
+
+void setup() {
+  
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass, "blynk.cloud",80);
+  
+
+}
+
+void loop() {
+  Blynk.run();
+
+}
+
+
