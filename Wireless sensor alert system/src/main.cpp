38181f330cd47abecec62d6cdc2960a432529fc5
Muaaz <muaazamjad007@gmail.com>
2026-04-14 21:55:49 +0200

    changed baudrate of platform IO serial monitor to 115200 same as MC. Update main to blink led on button press. Add serial monitor as well. Finished week 1 todos

--- Wireless sensor alert system/src/main.cpp
+++ Wireless sensor alert system/src/main.cpp
@@ -1,19 +1,33 @@
 #include <Arduino.h>
 
 // put function declarations here:
-
+PinStatus lastPinStatus = HIGH;
 void setup()
 {
   // Set Port 1 Pin 2 to Output
+
+  Serial.begin(115200);
   R_PORT1->PDR_b.PDR2 = 1;
+  // Set PIN D2 To input and enable pull up resistor (High potential)
+  pinMode(PIN_D2, INPUT_PULLUP);
 }
 
 void loop()
 {
-  // SET led state to High
-  R_PORT1->PODR_b.PODR2 = 1;
-  delay(1000);
-  // SET led state to Low
-  R_PORT1->PODR_b.PODR2 = 0;
-  delay(1000);
+
+  PinStatus pinStatus = digitalRead(PIN_D2);
+  // pinStatus Low = Button pressed
+  if (pinStatus == 0 && pinStatus != lastPinStatus)
+  {
+    R_PORT1->PODR_b.PODR2 = 1;
+    lastPinStatus = pinStatus;
+    Serial.println("LED is on");
+  }
+  // pinStatus High = Button not pressed
+  else if (pinStatus == 1 && pinStatus != lastPinStatus)
+  {
+    R_PORT1->PODR_b.PODR2 = 0;
+    lastPinStatus = pinStatus;
+  }
+  delay(50);
 }
